56ffc85eeb15dd83818e310216f0e0459470fb98
wkups <witold.kups@interlan.pl>
2017-12-21 17:13:15 +0100

    resolved package security issues

--- frontend/src/app/functional/ggmaps/directions-map.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DirectionsMapDirective } from './directions-map.directive';
-
-describe('DirectionsMapDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DirectionsMapDirective();
-    expect(directive).toBeTruthy();
-  });
-});
