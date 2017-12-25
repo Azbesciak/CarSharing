b48d3c714254272e2b9b9d6b4a30a5243854cc1e
Azbesciak <witkups@gmail.com>
2017-12-25 07:16:12 +0100

    Optimization at directions directive

--- frontend/src/app/functional/route/directions/directions-map.directive.ts
+++ frontend/src/app/functional/route/directions/directions-map.directive.ts
@@ -1,14 +1,14 @@
-import {Directive, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {GoogleMapsAPIWrapper} from "@agm/core";
-import { } from 'googlemaps';
-import {Route} from "../route";
-import {RouteWatcher} from "../route-watcher";
+import { Directive, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { GoogleMapsAPIWrapper } from "@agm/core";
+import {} from "googlemaps";
+import { Route } from "../route";
+import { RouteWatcher } from "../route-watcher";
 
 @Directive({
-  selector: 'agm-directions',
+  selector: "agm-directions",
 
 })
-export class DirectionsMapDirective extends RouteWatcher implements OnInit{
+export class DirectionsMapDirective extends RouteWatcher implements OnInit {
 
   @Input()
   directionsDisplay;
@@ -16,74 +16,83 @@ export class DirectionsMapDirective extends RouteWatcher implements OnInit{
   @Output()
   onDistanceChange = new EventEmitter<number>();
 
-  constructor(private gmapsApi: GoogleMapsAPIWrapper) {super()}
+  private lastTimeout;
+  private directionsService;
+
+  constructor(private gmapsApi: GoogleMapsAPIWrapper) {
+    super()
+  }
 
   ngOnInit() {
-    this.subscribe()
+    this.subscribe();
+    this.registerListener();
+    this.directionsService = new google.maps.DirectionsService;
   }
 
   onChange(route: Route) {
-    this.updateDirections(this.directionsDisplay)
+    this.updateDirections()
   }
 
   // updateDirections has 2 optional parameters. gets called in map.component
-  updateDirections(directionsDisplay, o?, d?) {
-    this.gmapsApi.getNativeMap().then(map => {
-      const directionsService = new google.maps.DirectionsService;
-      directionsDisplay.addListener('directions_changed', () => {
-        this.computeTotalDistance(this.directionsDisplay.getDirections());
+  updateDirections(o?, d?) {
+    clearTimeout(this.lastTimeout);
+    this.lastTimeout = setTimeout(() => {
+      this.gmapsApi.getNativeMap().then(map => {
 
-        if(directionsDisplay && directionsDisplay.dragResult){
-          let requestOrigin = directionsDisplay.dragResult.request.origin;
-          let requestDestination = directionsDisplay.dragResult.request.destination;
-          if(this.route.origin &&
-            ( requestOrigin.lat.toString() == this.route.origin.latitude &&
-              requestOrigin.lng.toString() == this.route.origin.longitude)){
-            let temp_lat = requestDestination.lat.toString();
-            let temp_lng = requestDestination.lng.toString();
-            this.route.origin.latitude = temp_lat;
-            this.route.origin.longitude = temp_lng;
-            this.updateDirections(directionsDisplay);
-          }
+        //if origin / destination are undefined use value from optional parameters.
+        this.directionsDisplay.setMap(map);
+        if (!this.route.destination && d) {
+          this.route.destination = d;
         }
-      });
+        if (!this.route.origin && o) {
+          this.route.origin = o;
+        }
 
-
-      //if origin / destination are undefined use value from optional parameters.
-      directionsDisplay.setMap(map);
-      if(!this.route.destination && d) {
-        this.route.destination = d;
-      }
-      if(!this.route.origin && o) {
-        this.route.origin  = o;
-      }
-
-      // give the route the data, travel mode is driving bc users should plan a camping/ roadtrip.
-      const wayPoints = this.route.wayPoints ? this.route.wayPoints.map(point => {
-        return {location: {lat: point.latitude, lng: point.longitude}, stopover: true}
+        // give the route the data, travel mode is driving bc users should plan a camping/ roadtrip.
+        const wayPoints = this.route.wayPoints ? this.route.wayPoints.map(point => {
+          return {location: {lat: point.latitude, lng: point.longitude}, stopover: true}
         }) : [];
-      directionsService.route({
-        origin: {lat: this.route.origin.latitude, lng: this.route.origin.longitude},
-        destination: {lat: this.route.destination.latitude, lng: this.route.destination.longitude},
-        waypoints: wayPoints,
-        optimizeWaypoints: true,
-        travelMode: google.maps.TravelMode.DRIVING
-      }, (response, status) => {
-        if(status == google.maps.DirectionsStatus.OK){
-            directionsDisplay.setDirections(response);
+        this.directionsService.route({
+          origin: {lat: this.route.origin.latitude, lng: this.route.origin.longitude},
+          destination: {lat: this.route.destination.latitude, lng: this.route.destination.longitude},
+          waypoints: wayPoints,
+          optimizeWaypoints: true,
+          travelMode: google.maps.TravelMode.DRIVING
+        }, (response, status) => {
+          if (status == google.maps.DirectionsStatus.OK) {
+            this.directionsDisplay.setDirections(response);
           } else {
-            window.alert('Directions request failed due to ' + status);
+            window.alert("Directions request failed due to " + status);
           }
+        });
       });
+    }, 200)
+  }
 
-      //function for displaying the travel distance
+  registerListener() {
+    this.directionsDisplay.addListener("directions_changed", () => {
+      this.computeTotalDistance(this.directionsDisplay.getDirections());
 
+      if (this.directionsDisplay && this.directionsDisplay.dragResult) {
+        let requestOrigin = this.directionsDisplay.dragResult.request.origin;
+        let requestDestination = this.directionsDisplay.dragResult.request.destination;
+        if (this.route.origin &&
+          (requestOrigin.lat.toString() == this.route.origin.latitude &&
+            requestOrigin.lng.toString() == this.route.origin.longitude)) {
+          let temp_lat = requestDestination.lat.toString();
+          let temp_lng = requestDestination.lng.toString();
+          this.route.origin.latitude = temp_lat;
+          this.route.origin.longitude = temp_lng;
+          this.updateDirections(this.directionsDisplay);
+        }
+      }
     });
   }
+
   computeTotalDistance(result) {
     const total = result.routes[0].legs
-            .map(r => r.distance.value)
-            .reduce((a, b) => a + b);
+      .map(r => r.distance.value)
+      .reduce((a, b) => a + b);
     this.onDistanceChange.next(total / 1000);
   }
 }
