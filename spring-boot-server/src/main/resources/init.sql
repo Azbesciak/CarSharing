0af8507d1bcd763c329899df65ba223c41038dcd
Witold Kupś <witold.kups@interlan.pl>
2018-01-01 10:05:39 +0100

    Added max passengers number function

--- spring-boot-server/src/main/resources/init.sql
+++ spring-boot-server/src/main/resources/init.sql
@@ -1,2 +1,22 @@
-insert into carScharing.dbo.[role] ([role]) VALUES ('ADMIN')
-insert into carScharing.dbo.[role] ([role]) VALUES ('USER')
+INSERT INTO carScharing.dbo.[role] ([role]) VALUES ('ADMIN')
+INSERT INTO carScharing.dbo.[role] ([role]) VALUES ('USER')
+
+
+CREATE FUNCTION dbo.RestrictNumberOfPassengers(@RoutePartId INT)
+  RETURNS INT AS
+  BEGIN
+    RETURN (
+             SELECT seat_count
+             FROM dbo.cars c, dbo.routes r, dbo.route_parts rp
+             WHERE rp.route_part_id = @RoutePartId AND rp.route_id = r.route_id AND c.car_id = r.car_id
+           )
+           -
+           (
+             SELECT COUNT(user_id)
+             FROM dbo.route_part_passengers
+             WHERE route_part_id = @RoutePartId
+           )
+  END;
+
+ALTER TABLE dbo.route_part_passengers
+  ADD CONSTRAINT CK_ROUTE_PARTS_PASSENGERS_MAX CHECK (dbo.RestrictNumberOfPassengers(route_part_id) > 0);
